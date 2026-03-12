22f092efbe7e4884ff2189f5326e9d4d8d547ad0
louisjasonsundoro <55065518+louisjasonsundoro@users.noreply.github.com>
2026-03-12 15:55:24 +0700

    Update sanity.ts

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -5,7 +5,7 @@ export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
   apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION!,
-  useCdn: false,
+  useCdn: true,
   token: process.env.SANITY_API_TOKEN,
 });
 
