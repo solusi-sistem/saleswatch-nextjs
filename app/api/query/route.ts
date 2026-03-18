8cf7ed2a22d70fd1619493334491c66c19e29acf
louisjasonsundoro <55065518+louisjasonsundoro@users.noreply.github.com>
2026-03-19 00:10:55 +0700

    Update route.ts

--- app/api/query/route.ts
+++ app/api/query/route.ts
@@ -1,12 +1,18 @@
 import { NextResponse } from 'next/server';
 import { client } from '@/lib/sanity';
 
+export const revalidate = 86400;
+
 export async function POST(request: Request) {
   try {
     const { query, params } = await request.json();
     
     // execute fetch on server
-    const data = await client.fetch(query, params || {});
+    const data = await client.fetch(query, params || {}, {
+      next: { 
+        revalidate: 86400 // 24 hours caching on Vercel
+      }
+    });
     
     // return data to browser
     return NextResponse.json(data);
