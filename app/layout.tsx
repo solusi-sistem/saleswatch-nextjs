beec388b0ce3a9355e2314bce6107a2a9182a2dc
louisjasonsundoro <55065518+louisjasonsundoro@users.noreply.github.com>
2026-03-19 11:02:57 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
@@ -42,6 +43,8 @@ export default function RootLayout({
             </PrivacyPolicyProvider>
           </HomeProvider>
         </LayoutProvider>
+        
+        <Analytics />
 
         {/* <Script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></Script> */}
       </body>
