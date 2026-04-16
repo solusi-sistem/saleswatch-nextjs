6cc9f245e4fd052dded69acfb8e02d522c7f0626
louisjasonsundoro <55065518+louisjasonsundoro@users.noreply.github.com>
2026-04-16 21:25:14 +0700

    Update sanity.ts

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -19,6 +19,9 @@ const builder = imageUrlBuilder({
 });
 
 export function urlFor(source: any) {
+  if (typeof source === 'string' && source.includes('cdn.sanity.io')) {
+    return source.replace('cdn.sanity.io', 'img.saleswatch.id');
+  }
   return builder.image(source);
 }
 
