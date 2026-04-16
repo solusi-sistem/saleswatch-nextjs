8d2d17d1f146e2faf8e50769f7db93d7697d3834
louisjasonsundoro <55065518+louisjasonsundoro@users.noreply.github.com>
2026-04-16 16:57:18 +0700

    Update sanity.ts

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -15,7 +15,7 @@ export const client = createClient({
 const builder = imageUrlBuilder(client);
 
 export function urlFor(source: any) {
-  return builder.image(source).auto('format').fit('max').host('img.saleswatch.id');
+  return builder.image(source).auto('format').fit('max').withConfig({ host: 'img.saleswatch.id' });
 }
 
 // ==========================================
