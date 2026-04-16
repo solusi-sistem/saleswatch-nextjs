c144c8fbaf2e2c339f085a2d319c4bf8a5d5bd77
louisjasonsundoro <55065518+louisjasonsundoro@users.noreply.github.com>
2026-04-16 16:48:44 +0700

    Update sanity.realtime.ts

--- lib/sanity.realtime.ts
+++ lib/sanity.realtime.ts
@@ -8,7 +8,6 @@ export const client = createClient({
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
   apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION!,
   useCdn: true,
-  assetHost: 'img.saleswatch.id',
   // token: process.env.SANITY_API_TOKEN,
 });
 
